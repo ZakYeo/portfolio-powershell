3293282e45365557dee9bb5eac6b64bebcf74cee
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-03 15:07:45 +0000

    Begun work on "Portfolio PowerShell"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.style.height = '100%';
+    document.body.style.margin = '0';
+    document.body.style.height = '100%';
+    document.body.style.backgroundColor = 'black';
+
+    return () => {
+      document.documentElement.style.height = null;
+      document.body.style.margin = null;    
+      document.body.style.height = null;
+      document.body.style.backgroundColor = null;
+    };
+  }, []);
+
+  const URL = document.location.href;
+
   return (
-    <div className="App">
-        Text
+    <div style={{width: '100%', height: '100%', backgroundColor: 'black', color: 'white'}}>
+      <div>
+        <div>Portfolio PowerShell</div>
+        <div>Copyright (C) Zak Yeomanson. All rights reserved.</div>
+      </div>
+      <div>{URL}> </div>
     </div>
   );
 }
