f9c4420c7992fd660a28baf71c9fefbbed60a877
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-03 16:45:39 +0000

    Text input now always focused

--- src/App.js
+++ src/App.js
@@ -49,6 +49,22 @@ function App() {
   const editableRef = useRef(null);
   const hiddenTextRef = useRef(null);
   const [caretLeft, setCaretLeft] = useState(0);
+  let lastSelectionRange = null;
+
+  const saveSelection = () => {
+    const selection = window.getSelection();
+    if (selection.rangeCount > 0 && editableRef.current.contains(selection.anchorNode)) {
+      lastSelectionRange = selection.getRangeAt(0);
+    }
+  };
+
+  const restoreSelection = () => {
+    const selection = window.getSelection();
+    if (lastSelectionRange && editableRef.current.contains(selection.anchorNode)) {
+      selection.removeAllRanges();
+      selection.addRange(lastSelectionRange);
+    }
+  };
 
   useEffect(() => {
     styleBody();
@@ -81,9 +97,21 @@ function App() {
       if (editableRef.current.contains(e.target)) {
         // Update caret on next tick to capture the new cursor position after the click
         setTimeout(updateCaretPosition, 0);
+      } else{
+        restoreSelection();
       }
     };
-  
+
+    const handleDocumentClick = (e) => {
+      if (!editableRef.current.contains(e.target)) {
+        editableRef.current.focus();
+        restoreSelection();
+      }
+    };
+    
+    editableRef.current.addEventListener('focus', restoreSelection);
+    editableRef.current.addEventListener('blur', saveSelection);
+    document.addEventListener('click', handleDocumentClick);
     editableRef.current.addEventListener('input', handleInput);
     editableRef.current.addEventListener('keydown', handleKeydown);
     editableRef.current.addEventListener('click', handleClick);
@@ -91,6 +119,9 @@ function App() {
   
     return () => {
       resetBodyStyle();
+      document.removeEventListener('click', handleDocumentClick);
+      editableRef.current.removeEventListener('focus', restoreSelection);
+      editableRef.current.removeEventListener('blur', saveSelection);
       editableRef.current && editableRef.current.removeEventListener('input', handleInput);
       editableRef.current && editableRef.current.removeEventListener('keydown', handleKeydown);
       editableRef.current && editableRef.current.removeEventListener('click', handleClick);
