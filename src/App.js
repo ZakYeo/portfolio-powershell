ea6e80fa39240e1d6cff6a553914e178808e1dfa
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-03 15:15:54 +0000

    Refactoring. Added two new functions & moved styling around

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
+
+const styles = {
+
+}
+
 function App() {
 
   useEffect(() => {
-    document.documentElement.style.height = '100%';
-    document.body.style.margin = '0';
-    document.body.style.height = '100%';
-    document.body.style.backgroundColor = 'black';
-
-    return () => {
-      document.documentElement.style.height = null;
-      document.body.style.margin = null;    
-      document.body.style.height = null;
-      document.body.style.backgroundColor = null;
-    };
+    styleBody();
+    return () => resetBodyStyle();
   }, []);
 
   const URL = document.location.href;
 
   return (
-    <div style={{width: '100%', height: '100%', backgroundColor: 'black', color: 'white'}}>
-      <div>
+    <div>
+      <div >
         <div>Portfolio PowerShell</div>
         <div>Copyright (C) Zak Yeomanson. All rights reserved.</div>
       </div>
-      <div>{URL}> </div>
+      <div>{URL}&gt; </div>
     </div>
   );
+}
+
+const resetBodyStyle = () => {
+  /** Reset the body style to its default */
+  document.documentElement.style.height = null;
+  document.body.style.margin = null;    
+  document.body.style.height = null;
+  document.body.style.backgroundColor = null;
+
+}
+
+const styleBody = () => {
+  /** Some web browsers have default margin and padding on the body element.
+   * Let's remove that & fill it black */
+  document.documentElement.style.height = '100%';
+  document.body.style.margin = '0';
+  document.body.style.height = '100%';
+  document.body.style.backgroundColor = 'black';
+  document.body.style.color = 'white'
 }
 
 export default App;
