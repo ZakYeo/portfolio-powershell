a1bc5576e8a4607081adc32166ef69bdc270775f
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-11 21:31:59 +0000

    Added cd command to help

--- src/variables.js
+++ src/variables.js
@@ -2,4 +2,5 @@ export const HELP_CMD_OUTPUT = [
   "Welcome to Zak's Website!",
   "You navigate this website by entering Terminal commands, just like you would a regular Terminal. Don't worry, it's easy! Here's a list of commands you can try:",
   "help -> View command list",
+  "cd -> Change directory",
 ];
