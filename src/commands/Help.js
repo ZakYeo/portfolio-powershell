01bb91fcf70fcb81b3272c14517e5dfca4315638
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-11 20:43:04 +0000

    Updated styling for help command

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -2,11 +2,19 @@ import { HELP_CMD_OUTPUT } from "../variables";
 
 const Help = () => {
   return (
-    <>
-      <div>{HELP_CMD_OUTPUT[0]}</div>
-      <div>{HELP_CMD_OUTPUT[1]}</div>
-      <div>{HELP_CMD_OUTPUT[2]}</div>
-    </>
+    <div
+      style={{
+        gap: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        paddingTop: "0.5rem",
+        paddingBottom: "0.5rem",
+      }}
+    >
+      {HELP_CMD_OUTPUT.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
   );
 };
 
