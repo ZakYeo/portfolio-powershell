ac17e03eb1e0ac2109b83279b0d613f5c45eeb75
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-11 14:20:09 +0000

    Added 'help' message

--- src/components/PowershellTitle.js
+++ src/components/PowershellTitle.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const PowershellTitle = () => {
-
-    return (
-        <div style={styles.titleText}>
-            <div>Portfolio PowerShell</div>
-            <div>Copyright (C) Zak Yeomanson. All rights reserved.</div>
-        </div>
-    )
-}
+  return (
+    <>
+      <div style={styles.titleText}>
+        <div>Portfolio PowerShell</div>
+        <div>Copyright (C) Zak Yeomanson. All rights reserved.</div>
+      </div>
+      <div>Type 'help' to get started.</div>
+    </>
+  );
+};
 
 const styles = {
-    titleText: {
-      paddingBottom: '1rem',
-    },
-}
+  titleText: {
+    paddingBottom: "1rem",
+  },
+};
 
 export default PowershellTitle;
