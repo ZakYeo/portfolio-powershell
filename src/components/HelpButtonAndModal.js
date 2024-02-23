d18289f4f9aa7d8edae625dcded7fd2a385c79f2
ZakYeo <55539592+ZakYeo@users.noreply.github.com>
2024-02-23 08:51:42 +0000

    Added styling & help message to the modal

--- src/components/HelpButtonAndModal.js
+++ src/components/HelpButtonAndModal.js
@@ -1,22 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { FloatButton, Modal, Button } from 'antd'
 import { QuestionCircleOutlined } from '@ant-design/icons';
-
+import '../styles/HelpButtonAndModal.css';
 const HelpButtonAndModal = () => {
     const [helpModal, setHelpModalOpen] = useState(false);
 
     return (
         <>
             <Modal
-                title="Help"
+                title="What Is This And Where Am I?"
                 open={helpModal}
                 onOk={() => setHelpModalOpen(false)}
+                wrapClassName="custom-modal"
+                centered
                 footer={[
                     <Button key="submit" type="primary" onClick={() => setHelpModalOpen(false)}>
                         OK
                     </Button>,
                 ]}
-            />
+            >
+                <p>Welcome to my website!</p>
+                <p>Here, you will find that this is not your traditional website. <br /> To navigate this website, you must enter a command into the input, and press enter. <br />Don't worry, it's easy! Start by trying the command 'help'. </p>
+                <p>Once you get comfortable, you can find information about myself & fun & interesting features I've implemented into this site.</p>
+                <p>Enjoy!</p>
+            </Modal>
             <FloatButton icon={<QuestionCircleOutlined />} onClick={() => setHelpModalOpen(true)} />
         </>
     );
