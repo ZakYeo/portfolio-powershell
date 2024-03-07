11ce7983588479feb40ab9a3c929ce41ed90bcb0
ZakYeo <zakyeomanson@googlemail.com>
2024-03-07 08:54:16 +0000

    Updated help command formatting

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -1,24 +1,28 @@
+import React from 'react';
 import { commands } from "../util/handleCommands";
 
 const Help = () => {
   // Convert commands object to an array, sort it by command name, then map for display
   const commandHelpOutput = Object.keys(commands)
     .sort() // Sort command names alphabetically
-    .map(key => `${key} - ${commands[key].description}`);
+    .map(key => ({
+      command: key,
+      description: commands[key].description,
+    }));
 
   return (
-    <div
-      style={{
-        gap: "1rem",
-        display: "flex",
-        flexDirection: "column",
-        paddingTop: "0.5rem",
-        paddingBottom: "0.5rem",
-      }}
-    >
-      {commandHelpOutput.map((item, index) => (
-        <div key={index}>{item}</div>
+    <div style={{ padding: '1rem', maxWidth: '50rem' }}>
+      <div style={{ borderBottom: '1px solid white', margin: '0.5rem 0' }}></div> {/* Top strikethrough */}
+      {commandHelpOutput.map((item, index, array) => (
+        <React.Fragment key={index} >
+          <div style={{ display: 'flex', paddingBottom: '0.5rem' }}>
+            <span style={{ marginRight: '1rem', width: '20%', minWidth: '7rem' }}>{item.command}</span>
+            <span>{item.description}</span>
+          </div>
+          {index < array.length - 1 && <div style={{ borderBottom: '1px solid white', margin: '0.5rem 0' }}></div>}
+        </React.Fragment>
       ))}
+      <div style={{ borderBottom: '1px solid white', margin: '0.5rem 0' }}></div> {/* Bottom strikethrough */}
     </div>
   );
 };
